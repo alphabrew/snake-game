6e96fc5dd6df506d26ef78e1c366f42aa925a24e
alphabrew <steven.harbin@gmail.com>
2021-03-07 09:14:41 -0500

    updates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 
 snake= Snake()
+food= Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,6 +25,10 @@ while game_on:
     screen.update()
     time.sleep(.1)
     snake.move()
+
+    #detect collision with Food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 
